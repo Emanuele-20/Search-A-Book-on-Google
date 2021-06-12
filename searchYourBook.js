d0ad9687eeb15cf8d39a7c8dd0a51de631aa2b3a
Emanuele Pace <emanuele.10@outlook.it>
2021-06-12 11:13:11 +0100

    Add functionality into my function to receive back books object

--- searchYourBook.js
+++ searchYourBook.js
@@ -1,8 +1,13 @@
+const fetch = require('node-fetch');
 
 function getYourBook(){
     fetch('https://www.googleapis.com/books/v1/volumes?q=harrypotter')
+    .then(answer => {
+        return answer.json()
+    })
     .then(answer => {
         console.log(answer)
     })
 }
 
+getYourBook()
