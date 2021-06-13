8184a58ec7fee4bfa34e5b906e0d0a456216f569
Emanuele Pace <emanuele.10@outlook.it>
2021-06-13 17:02:10 +0100

    Async is working and can filter the first 5 books

--- searchYourBook.js
+++ searchYourBook.js
@@ -1,16 +1,16 @@
 const fetch = require('node-fetch');
 
 async function getYourBook(){
-    const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=harrypotter')
+    const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=pinocchio')
     const data = await resp.json()
-    const filter =  await data.items.forEach(el => {
-        console.log(el.volumeInfo.title)
-        console.log(el.volumeInfo.authors[0])
-        console.log(el.volumeInfo.publisher)
+    const filter = await data.items.slice(0,5)
+        .forEach(element => {
+        console.log(element.volumeInfo.title)
+        console.log(element.volumeInfo.authors[0])
+        console.log(element.volumeInfo.publisher)
         console.log('\n')
     })
     return filter
-    
 }
     // .catch(answer => console.log("Ops, I can't reach this information"))
     
