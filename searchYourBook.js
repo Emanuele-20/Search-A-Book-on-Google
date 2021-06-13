af0c7d011c53ca059cf0f4f09d830797e322e629
Emanuele Pace <emanuele.10@outlook.it>
2021-06-13 17:20:24 +0100

    Add try and catch keyword and manage the error

--- searchYourBook.js
+++ searchYourBook.js
@@ -1,16 +1,20 @@
 const fetch = require('node-fetch');
 
-async function getYourBook(){
-    const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=pinocchio')
-    const data = await resp.json()
-    const filter = await data.items.slice(0,5)
-        .forEach(element => {
-        console.log(element.volumeInfo.title)
-        console.log(element.volumeInfo.authors[0])
-        console.log(element.volumeInfo.publisher)
-        console.log('\n')
-    })
-    return filter
+async function getYourBook(title){
+    try{
+        const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + title)
+        const data = await resp.json()
+        const filter = await data.items.slice(0,5)
+            .forEach(element => {
+            console.log('Title:', element.volumeInfo.title)
+            console.log('Author:',element.volumeInfo.authors[0])
+            console.log('Publishing Company:',element.volumeInfo.publisher)
+            console.log('\n')
+        })
+        return filter
+    } catch (err){
+        console.log("Opss, somethind did wrong", err)
+    } 
 }
     // .catch(answer => console.log("Ops, I can't reach this information"))
     
