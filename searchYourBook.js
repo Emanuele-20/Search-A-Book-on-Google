9f41ed6152b2f929532bfa3df7bd63038137aedf
Emanuele Pace <emanuele.10@outlook.it>
2021-06-14 17:01:22 +0100

    Added a "while loop" to manage the different user options

--- searchYourBook.js
+++ searchYourBook.js
@@ -20,36 +20,34 @@ async function getYourBook(title){
 
 function populateReadingList(index){
     readingList.push(books[index])
-    console.log("Your selection is saved") 
+    books.length = 0
+    console.log("Your selection is saved")
 }
 
-
-// async function start(){
-
-//     let book = readlineSync.question("Hello, which book are you looking for? \n")
-//     await getYourBook(book)
-//     let index = readlineSync.keyInSelect(books, "Which book do you want to save in your reading list?")
-//     await populateReadingList(index)
-//     //console.log(`Your Reading List: ${readingList.join("\n")}`)
-
-// }
 
 async function start(){
 
-    let question = await readlineSync.question("Hello, select 1 to search a book or select 2 to view yor reading list? \n")
-        if (question === "1" ){
-            let book = readlineSync.question("Which book are you looking for? \n")
+    while(true){
+        let question = await readlineSync.question("Hello, select 1 to search a book, select 2 to view yor Reading List, 'exit' to interrupt? \n")
+        if (question === "1"){
+            let book = await readlineSync.question("Which book are you looking for? \n")
             await getYourBook(book)
             let index = readlineSync.keyInSelect(books, "Which book do you want to save in your reading list?")
             await populateReadingList(index)
-        } else if (question === "2") {   
+        }else if (question === "2") {   
             if (readingList.length == 0){
                 console.log("Your reading list is empty")
             } else {
-            console.log(`Your Reading List: ${readingList.join("\n")}`)
+                console.log(`Your Reading List:\n${readingList.join("\n")}`)
+            }
+        } else {
+            if (question === "exit"){
+                break;
             }
         }
-        
-}
+    } 
 
-start()
+};
+
+
+start();
