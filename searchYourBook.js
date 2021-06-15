1fd77f2b5b5491175d4c863a0cd86b161e598e8d
Emanuele Pace <emanuele.10@outlook.it>
2021-06-15 09:55:41 +0100

    Add if statement to manage the users selections

--- searchYourBook.js
+++ searchYourBook.js
@@ -8,33 +8,32 @@ async function getYourBook(title){
     try {
         const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + title)
         const data = await resp.json()
-        await data.items.slice(0,5)
+        data.items.slice(0,5)
             .forEach(element => {
             books.push(`Title: ${element.volumeInfo.title} -  `+ `Author: ${element.volumeInfo.authors[0]} -  `+ `Publishing Company: ${element.volumeInfo.publisher}`)
         })
-        //console.log(books.join("\n"))
     } catch (err){
         console.log("Opss, something did wrong", err)
     } 
 }
 
-function populateReadingList(index){
-    readingList.push(books[index])
+function populateReadingList(selectedBook){
+    readingList.push(books[selectedBook])
     books.length = 0
-    console.log("Your selection is saved")
+    return ("Your selection is saved")
 }
 
 
 async function start(){
 
     while(true){
-        let question = await readlineSync.question("Hello, select 1 to search a book, select 2 to view yor Reading List, 'exit' to interrupt? \n")
+        const question = readlineSync.question("Hello, select 1 to search a book, select 2 to view yor Reading List, 'exit' to interrupt? \n")
         if (question === "1"){
-            let book = await readlineSync.question("Which book are you looking for? \n")
+            const book =  readlineSync.question("Which book are you looking for? \n")
             await getYourBook(book)
-            let index = readlineSync.keyInSelect(books, "Which book do you want to save in your reading list?")
-            await populateReadingList(index)
-        }else if (question === "2") {   
+            const selectedBook = readlineSync.keyInSelect(books, "Which book do you want to save in your reading list?")
+            populateReadingList(selectedBook)
+        } else if (question === "2") {   
             if (readingList.length == 0){
                 console.log("Your reading list is empty")
             } else {
