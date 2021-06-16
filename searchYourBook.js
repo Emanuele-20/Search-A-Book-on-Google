8b30ee869f5564da05d2c7c787a5a8a5c5423c48
Emanuele Pace <emanuele.10@outlook.it>
2021-06-16 09:51:35 +0100

    Small refactor

--- searchYourBook.js
+++ searchYourBook.js
@@ -8,9 +8,8 @@ async function getYourBook(title){
     try {
         const resp = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + title)
         const data = await resp.json()
-        data.items.slice(0,5)
-            .forEach(element => {
-            books.push(`Title: ${element.volumeInfo.title} -  `+ `Author: ${element.volumeInfo.authors[0]} -  `+ `Publishing Company: ${element.volumeInfo.publisher}`)
+        data.items.slice(0,5).forEach(element => {
+            books.push(`Title: ${element.volumeInfo.title} - `+ `Author: ${element.volumeInfo.authors[0]} - `+ `Publishing Company: ${element.volumeInfo.publisher}`)
         })
     } catch (err){
         console.log("Opss, something did wrong", err)
@@ -20,21 +19,22 @@ async function getYourBook(title){
 function populateReadingList(selectedBook){
     readingList.push(books[selectedBook])
     books.length = 0
-    console.log("This book is saved in your reading list, thank you")
+    console.log("Thank you for your selection")
 }
 
 
 async function start(){
 try{
+    console.log("Hello and Welcome")
     while(true){
-        const question = readlineSync.question("Hello, select 1 to search a book, select 2 to view yor Reading List, 'exit' to interrupt?\n")
+        const question = readlineSync.question("Please, select 1 to search a book"+ "select 2 to view yor Reading List, digit 'exit' to interrupt?\n")
         if (question === "1"){
             const book =  readlineSync.question("Which book are you looking for?\n")
             await getYourBook(book) 
             const selectedBook = readlineSync.keyInSelect(books, "Which book do you want to save in your reading list?\n")
             populateReadingList(selectedBook)
         } else if (question === "2") {    
-            if (readingList.length == 0){
+            if (readingList.length === 0){
                 console.log("Your reading list is empty")
             } else {
                 console.log(`Your Reading List:\n${readingList.join("\n")}`)
